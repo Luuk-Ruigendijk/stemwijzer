5583db455c31d8ff9210643c071b848e6ed53175
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2019-02-08 14:29:43 +0100

    put the menu in a div. added a background image. added css to the new menu div and the new background

--- index.php
+++ index.php
@@ -2,13 +2,16 @@
 <html>
 <head>
 	<title>Stemwijzer</title>
+	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body>
-<h1></h1>
-<p></p>
-<button style="visibility: hidden;" onclick="agree()">Eens</button><button style="visibility: hidden;" onclick="neither()">Geen van beide</button><button style="visibility: hidden;" onclick="disagree()">Oneens</button>
-<button onclick="start">Start</button>
-<span style="visibility: hidden;"><p>Wat vinden de partijen?</p></span>
+	<div id="menu">
+		<h1></h1>
+		<p></p>
+		<button style="visibility: hidden;" onclick="agree()">Eens</button><button style="visibility: hidden;" onclick="neither()">Geen van beide</button><button style="visibility: hidden;" onclick="disagree()">Oneens</button>
+		<button onclick="start">Start</button>
+		<span style="visibility: hidden;"><p>Wat vinden de partijen?</p></span>
+	</div>
 <script type="text/javascript" src="objects.js"></script>
 <script type="text/javascript" src="script.js"></script>
 </body>
