f12930b99e0a0a08ac178c9ec65e68a1735b46e6
LAPTOP-DCREO7LQ\luck4you <99046268@mydavinci.nl>
2019-02-08 08:46:07 +0100

    put objects in a new file, created the first functions, added some more HTML content.

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 	<title>Stemwijzer</title>
 </head>
 <body>
-
+<h1></h1>
+<p></p>
+<button style="visibility: hidden;" onclick="agree()">Eens</button><button style="visibility: hidden;" onclick="neither()">Geen van beide</button><button style="visibility: hidden;" onclick="disagree()">Oneens</button>
+<button onclick="start">Start</button>
+<span style="visibility: hidden;"><p>Wat vinden de partijen?</p></span>
+<script type="text/javascript" src="objects.js"></script>
+<script type="text/javascript" src="script.js"></script>
 </body>
 </html>
